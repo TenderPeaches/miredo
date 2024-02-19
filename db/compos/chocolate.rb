7dfefe0e77a28db75367c821411388de95317ed3
TenderPeaches <69730356+TenderPeaches@users.noreply.github.com>
2024-02-19 16:04:01 -0500

    Delete db/compos directory

--- db/compos/chocolate.rb
+++
@@ -1,4 +0,0 @@
-"keep eating chocolate boy
-how's that child labor tasting
-you got your rush yet
-"
--- db/compos/song_ideas.rb
+++
@@ -1,5 +0,0 @@
-to_script = [
-    "livin la vida loca" # lol
-]
-
-https://old.reddit.com/r/interestingasfuck/comments/1agjy1n/unfortunately_his_warnings_in_the_90s_have_come/
