cbf8f74deee9bfcab9631c30edb7f88030005f50
Ysaac Quevillon <tendrepeches@protonmail.com>
2024-03-05 19:38:12 -0500

    fix progression chords

--- app/controllers/progressions_controller.rb
+++ app/controllers/progressions_controller.rb
@@ -1,7 +1,7 @@
 class ProgressionsController < ApplicationController
     before_action :set_or_new_progression, only: %i[ add_chord ]
     before_action :set_progression, only: %i[ update destroy ]
-    
+
     def index
         # doesn't apply
     end
@@ -22,33 +22,33 @@ class ProgressionsController < ApplicationController
     def create
         @progression = Progression.new(progression_params)
         song = Song.find_by_id(progression_params[:song_id])
-        @progression_index = song.progressions.distinct.count + 1 
+        @progression_index = song.progressions.distinct.count + 1
 
         respond_to do |format|
-            if @progression.save 
+            if @progression.save
                 format.turbo_stream { render "songs/define_progressions/create_progression" }
             else
 
             end
         end
 
-        
-    end 
 
-    def update 
+    end
+
+    def update
         # through /songs/:id/define_progressions
         respond_to do |format|
             if @progression.update(progression_params)
                 format.html { render json: { success: 'yes' }, notice: "success" }
                 format.turbo_stream
-            else 
+            else
                 format.json { render json: @progression.errors, status: :unprocessable_entity }
                 format.turbo_stream { render plain: @progression.errors.full_messages, status: :unprocessable_entity }
             end
         end
     end
 
-    def destroy 
+    def destroy
         @progression.destroy
 
         render 'songs/define_progressions/destroy_progression'
@@ -73,7 +73,7 @@ class ProgressionsController < ApplicationController
         end
     end
 
-    def progression_params 
+    def progression_params
         params.require(:progression).permit(:tag, :uid, :scale, :key, :reps, :id, :song_id, progression_chords_attributes: [:id, :degree, :modifier, :bass_modifier, :bass_degree, :chord_id, :duration, :staccato, :muted, :_destroy] )
     end
 end
