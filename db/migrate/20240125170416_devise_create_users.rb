5dc36fab096ff8a19249fce33ac5b6f6d3ee9897
Poubelle <tendrepeches@protonmail.com>
2024-04-04 13:37:17 -0400

    user admin column

--- db/migrate/20240125170416_devise_create_users.rb
+++ db/migrate/20240125170416_devise_create_users.rb
@@ -3,6 +3,8 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.boolean :is_admin, null: false, default: false, comment: "Whether this user is a site administrator"
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
--- db/schema.rb
+++ db/schema.rb
@@ -288,6 +288,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_13_225536) do
     t.datetime "confirmation_sent_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
