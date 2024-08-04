07088685ea6b54ed34f4c465146a1293b0c17604
Poubelle <tendrepeches@protonmail.com>
2024-08-04 13:12:22 -0400

    locale revamp

---
+++ docs/meta.r
@@ -0,0 +1,11 @@
+locales:
+    keywords        # generic words used especially for buttons, navigation
+    actions         # CTAs under the "new {thing}", "show {thing}" format
+    confirm         # confirmation prompts
+    alerts          # feedback for invalid user actions
+    cta             # project-specific call-to-action labels 
+    hints           # hints are used to describe/define a specific phrase in the context of the website; since they can be used anywhere, they are considered generic and thus shouldn't be placed under the content hierarchy
+    controls        # is specifically about user inputs that adjust settings on the page #?  consider whether move to content
+    content         # project-specific content blurbs
+    notices         #! move to content 
+    activerecord    # ActiveRecord model labels 
