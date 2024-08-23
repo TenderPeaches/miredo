deb46dc896c7e197360ef0b42e1e953196acf09c
Poubelle <tendrepeches@protonmail.com>
2024-08-23 12:29:12 -0400

    user default instrument

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,27 @@
 class User < ApplicationRecord
-  has_many :submissions, class_name: "Song", inverse_of: :submitter, foreign_key: :submitter_id
-  has_many :user_favorites, dependent: :destroy
-  has_many :favorites, through: :user_favorites, source: :song
-  has_one :user_setting
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    has_many :submissions, class_name: "Song", inverse_of: :submitter, foreign_key: :submitter_id
+    has_many :user_favorites, dependent: :destroy
+    has_many :favorites, through: :user_favorites, source: :song
+    has_one :user_setting
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
 
-  def is_admin?
-      self.is_admin
-  end
+    def is_admin?
+        self.is_admin
+    end
 
-  # because humans typically pluralize settings
-  def user_settings
-      self.user_setting
-  end
+    # because humans typically pluralize settings
+    def user_settings
+        self.user_setting
+    end
+
+    def default_instrument
+        if user_settings
+            user_settings.default_instrument
+        else
+            Instrument.default
+        end
+    end
 end
