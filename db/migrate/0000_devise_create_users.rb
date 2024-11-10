d5f46056fb66eb54e9113dd938d7c35ee53ea85f
Poubelle <tendrepeches@protonmail.com>
2024-11-10 07:32:34 -0500

    add contributor attribute to user

--- db/migrate/0000_devise_create_users.rb
+++ db/migrate/0000_devise_create_users.rb
@@ -4,6 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.boolean :is_admin, null: false, default: false, comment: "Whether this user is a site administrator"
+      t.boolean :is_contributor, null: false, default: false, comment: "Whether this user is a Contributor (paying member)"
 
       ## Database authenticatable
       t.string :email,              null: false, default: ""
