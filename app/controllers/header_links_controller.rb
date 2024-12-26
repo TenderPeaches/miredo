37ca28359068e3297c8441e4e08a0047ab6faadd
Poubelle <tendrepeches@protonmail.com>
2024-12-26 13:13:18 -0500

    keep header links state throughout navigation

--- app/controllers/header_links_controller.rb
+++ app/controllers/header_links_controller.rb
@@ -1,5 +1,9 @@
 class HeaderLinksController < ApplicationController
     def index
+        # whether the links should appear or be withdrawn from the screen
         @show = params[:show] == "true"
+
+        # update the user's session with the setting, so that it carries over from one page to the next; use true/nil dichotomy to minimize storage use and to prevent having to compare against "false" when checking the flag
+        session[:show_header_links] = if @show then true else nil end
     end
 end
--- changelog.md
+++ changelog.md
@@ -2,6 +2,10 @@
 
 ## Early Access
 
+## v1.0.3.2
+
+* Header links remain visible or hidden throughout the user's navigation
+
 ## v1.0.3.1
 
 * UI changes to the header's design, links and links throughout the site
