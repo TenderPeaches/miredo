a0362c059b2c1d49c5a41960ddec0759184ac56d
Poubelle <tendrepeches@protonmail.com>
2025-01-20 18:19:24 -0500

    fix inquiry mailer bug

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,6 +1,6 @@
 # contact us page
 class InquiriesController < ApplicationController
     def create
-        ContactMailer.with(return_address: params[:email], subject: params[:subject], body: params[:body]).inquiry.delivery_now
+        ContactMailer.with(return_address: params[:email], subject: params[:subject], body: params[:body]).inquiry.deliver_now
     end
 end
