4f6d0a71df6498fd6a1f09001e238c4e81bd6f24
Poubelle <tendrepeches@protonmail.com>
2025-01-21 00:55:16 -0500

    admin email swap

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ApplicationMailer
-    default from: Rails.configuration.admin_email
+    default from: Rails.configuration.inquiries_email
 
     def inquiry
         @return_address = params[:return_address]
         @subject = params[:subject]
         @body = params[:body]
 
-        mail(to: Rails.configuration.inquiries_email, subject: @subject)
+        mail(to: Rails.configuration.admin_email, subject: @subject)
     end
 end
